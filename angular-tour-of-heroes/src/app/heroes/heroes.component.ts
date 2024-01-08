5f874d61dcc4d20e108a29600d6f07ef9da0c9ef
Rajethan Ranjan <raji@niceapps.ch>
2024-01-08 15:25:25 +0100

    Implement heroes

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Hero } from '../../types/hero';
+import { UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [UpperCasePipe, FormsModule],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.scss',
+})
+export class HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+  };
+}
