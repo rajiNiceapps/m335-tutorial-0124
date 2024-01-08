88b45c790a6494f3e535c332613a169e18008008
Rajethan Ranjan <raji@niceapps.ch>
2024-01-08 15:33:37 +0100

    Implement list

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
-import { Hero } from '../../types/hero';
-import { UpperCasePipe } from '@angular/common';
+import { NgForOf, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../../data/mock-heroes';
+import { Hero } from '../../types/hero';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule],
+  imports: [UpperCasePipe, FormsModule, NgForOf, NgIf],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss',
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
